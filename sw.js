edcfca93ad80f480b809d48d85de5c4da5222c20
newbroman <105666230+newbroman@users.noreply.github.com>
2026-04-02 19:21:32 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE_NAME = 'polish-trainer-v9';
+const CACHE_NAME = 'polish-trainer-v10';
 const ASSETS = [
   'index.html',
-  'manifest.json'
+  'manifest.json',
   'icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', (e) => {
